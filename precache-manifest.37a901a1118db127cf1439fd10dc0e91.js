b2971bde2255e8af5c843816abf8195a93785e03
imidaho <jamesmcochran@gmail.com>
2020-03-04 15:07:32 -0500

    Updates

---
+++ precache-manifest.37a901a1118db127cf1439fd10dc0e91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94c3646ba082907881b45ef37b0085c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f5a41d7b59009bc2bd8",
+    "url": "/static/css/main.7ca377b6.chunk.css"
+  },
+  {
+    "revision": "09fc65cd15543e23a37a",
+    "url": "/static/js/2.f18f8138.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f18f8138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5a41d7b59009bc2bd8",
+    "url": "/static/js/main.7be2a0e8.chunk.js"
+  },
+  {
+    "revision": "ad9c5439a64956028448",
+    "url": "/static/js/runtime-main.f32cae03.js"
+  },
+  {
+    "revision": "fa11c3ca7b2301d6b8da51b04985fb05",
+    "url": "/static/media/BrandonGrotesque-Bold.fa11c3ca.otf"
+  },
+  {
+    "revision": "4ceed521794730211273871ff9fa01d3",
+    "url": "/static/media/JMC-Chow Lan Lake.4ceed521.jpg"
+  }
+]);
